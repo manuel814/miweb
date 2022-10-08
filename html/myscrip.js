b6ed23148fa8671ffb74509963bc6e101654cd4a
Manuel Chavez <manuelchavezgarcia1250@gmail.com>
2022-10-07 22:13:28 -0500

    mi primer commit

---
+++ html/myscrip.js
@@ -0,0 +1,11 @@
+function calculate(){
+    var principal = 0;
+    var rate = 0;
+    var year = 0;
+    var SI = 0;
+    principal = parseInt(document.getElementById("pr").value);
+    year = parseInt(document.getElementById("yr").value);
+    rate = parseInt(document.getElementById("roi").value);
+    si =(principal*year*rate)/100;
+    document.getElementById("res").innerHTML=SI;
+}
